7a32ec70afb80d934d1598461482262bbaf878a5
achahbar <shadys.50c@gmail.com>
2024-05-01 16:54:23 +0200

    changin api

--- src/components/api.js
+++ src/components/api.js
@@ -1,2 +1,2 @@
-const api = "https://bank-01-9dc728aeb614.herokuapp.com/" //"http://localhost:8080/"    //"https://bank-01-9dc728aeb614.herokuapp.com/"       
+const api = "http://93.127.203.203:8080/" //"https://bank-01-9dc728aeb614.herokuapp.com/" //"http://localhost:8080/"    //"https://bank-01-9dc728aeb614.herokuapp.com/"       
 export default api
